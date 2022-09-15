34e3f05864287d42f90c067e875f1b126ab57c57
nuey <krittiya.ker@ku.th>
2022-09-15 15:16:33 +0700

    first commit

---
+++ src/main/java/th/ac/ku/restaurant/model/User.java
@@ -0,0 +1,29 @@
+package th.ac.ku.restaurant.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    private String role;
+
+
+}
+
